43a9baae9c008167ae12e2e8d67f82eae8f32882
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2023-07-30 20:50:57 +0200

    Refacto code.

--- src/Exception/FileNotExistException.php
+++ src/Exception/FileNotExistException.php
@@ -9,6 +9,11 @@ final class FileNotExistException extends Exception
 {
     private const MESSAGE = 'File not exist.';
     public function __construct(?string $message = null, $code = 0, Throwable $previous = null) {
-        parent::__construct(null === $message ? self::MESSAGE : $message.' '.self::MESSAGE, $code, $previous);
+        parent::__construct($this->getCustomMessage($message), $code, $previous);
+    }
+
+    private function getCustomMessage(?string $message): string
+    {
+        return null === $message ? self::MESSAGE : $message.' '.self::MESSAGE;
     }
 }
