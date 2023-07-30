5adfb46044ea4219b873f454c51e8ddf0001d933
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2023-07-31 00:33:07 +0200

    Refactor code.

--- src/Service/ReadFile/ReadFile.php
+++ src/Service/ReadFile/ReadFile.php
@@ -7,7 +7,9 @@ namespace App\Service\ReadFile;
 use App\Service\CheckFile\CheckFile;
 use App\Service\ReadFile\Interface\ReadFileInterface;
 use App\Service\ReplaceEndOfLine\ReplaceEndOfLine;
+use ClosedGeneratorException;
 use Exception;
+use Generator;
 
 final class ReadFile implements ReadFileInterface
 {
@@ -28,15 +30,14 @@ final class ReadFile implements ReadFileInterface
      */
     public function read(?string $fileName): bool
     {
+        $fileName = $this->getFileName($fileName);
         $fileFullPath = $this->getFileFullPath($fileName);
         $this->checkFile->check($fileFullPath);
 
         $handle = fopen($fileFullPath, 'r');
         if ($handle) {
-            while (($line = fgets($handle)) !== false) {
-                $line = $this->replaceEndOfLine->replace($line);
-                var_dump($line);
-            }
+            $lines = $this->getLines($handle);
+            $this->saveFile($this->getNewFileFullPath($fileName), $lines);
             fclose($handle);
 
             return true;
@@ -46,7 +47,32 @@ final class ReadFile implements ReadFileInterface
 
     private function getFileFullPath(?string $fileName): string
     {
-        $fileName = $fileName ?? self::DEFAULT_FILE_NAME;
         return dirname(__DIR__, self::DIR_LEVEL_DOWN).self::RELATIVE_FILE_PATH.$fileName;
+    }
+
+    private function getNewFileFullPath(string $fileName): string
+    {
+        return $this->getFileFullPath(str_replace('.', '_new.', $fileName));
+    }
+
+    private function getLines($handle): Generator
+    {
+        while (($line = fgets($handle)) !== false)  {
+            yield $this->replaceEndOfLine->replace($line);
+        }
+    }
+
+    private function getFileName(?string $fileName): string
+    {
+        return $fileName ?? self::DEFAULT_FILE_NAME;
+    }
+
+    private function saveFile(string $fileName, Generator $body): void
+    {
+        $handle = fopen($fileName, 'w');
+            foreach ($body as $bod) {
+                fwrite($handle, $bod);
+            }
+        fclose($handle);
     }
 }
