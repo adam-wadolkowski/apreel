1e0f8fad969a78eba65131264f2a067172858c39
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2023-07-30 21:44:05 +0200

    Refactor code.

--- src/Service/ReplaceFile/ReplaceEndOfLine.php
+++ src/Service/ReplaceFile/ReplaceEndOfLine.php
@@ -6,8 +6,10 @@ use App\Service\ReplaceFile\Interface\ReplaceEndOfLineInterface;
 
 class ReplaceEndOfLine implements ReplaceEndOfLineInterface
 {
+    private const NO_UNIX_END_OF_LINE = ['\r', '\n'];
+    private const UNIX_END_OF_LINE = '\r\n';
     public function replace(string $line): string
     {
-        return str_replace(['\r', '\n'], '\r\n', $line);
+        return str_replace(self::NO_UNIX_END_OF_LINE, self::UNIX_END_OF_LINE, $line);
     }
 }
