7af9a98b7c8f1f64c7e1a27ed49f8bf70f269289
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2023-07-30 16:25:52 +0200

    Add logic read file by line.

--- src/File.php
+++ src/File.php
@@ -2,11 +2,39 @@
 
 namespace App;
 
+use Exception;
+
 final class File
 {
-    private const RELATIVE_FILE_PATH = 'file/test.txt';
+    private const RELATIVE_FILE_PATH = '/file/';
     
-    public function read(?string $fileName = 'test.txt')
+    public function read(?string $fileName = 'test.txt'): void
     {
+        $fileFullPath = dirname(__DIR__).self::RELATIVE_FILE_PATH.$fileName;
+
+        $this->checkFile($fileFullPath);
+
+        $handle = fopen($fileFullPath, 'r');
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                echo $line;
+            }
+        fclose($handle);
+        }
+    }
+
+    private function checkFile(string $fileFullPath): void
+    {
+        if (!file_exists($fileFullPath)) {
+            throw new Exception('File not exist.');
+        }
+
+        if (!is_file($fileFullPath)) {
+            throw new Exception('This is not a file.');
+        }
+
+        if (!is_readable($fileFullPath)) {
+            throw new Exception('File ins not readable.');
+        }
     }
 }
