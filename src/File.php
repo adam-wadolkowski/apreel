bd0ac5a262e12d7882d26befa13088e0777d5fb6
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2023-07-30 12:54:16 +0200

    Init commit.

---
+++ src/File.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+final class File
+{
+    private const RELATIVE_FILE_PATH = 'file/test.txt';
+    
+    public function read(?string $fileName = 'test.txt')
+    {
+    }
+}
---
+++ src/Run.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$file = new File();
+$file->read();
